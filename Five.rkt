06c5bae359ec067aad5eb12e0ed7a25ea2477a98
ngattusohw <ngattusohw@gmail.com>
2017-02-28 20:37:34 -0500

    Adding in-progress Lab 5 file

---
+++ Five.rkt
@@ -0,0 +1,37 @@
+#lang racket
+
+(define (pig-latin word)
+   (if (null? word)
+        '()
+        (if (equal? (car word) 'q)
+            (append (cddr word) (cons (car word) '(u a y)))
+            (append (cdr word) (cons (car word) '(a y))))))
+
+(define (Weight w)
+  (* 0.453592 w))
+
+(define (Height h)
+  (* 0.0254 h))
+
+(define (CalcBMI Weight Height)
+  (/ Weight (* Height Height)))
+         
+(define (BMI h w)
+  (define bmi (CalcBMI (Weight w) (Height h)))
+  (display bmi)
+  (cond ((> 16 bmi) (display "Severely Underweight"))
+      ((<= 16 bmi 18.5) (display "Underweight"))
+      ((<= 18.5 bmi 25) (display "Normal"))
+      ((<= 25 bmi 30) (display "Overweight"))
+      ((<= 30 bmi 40) (display "Obese"))
+      (else
+       (display "Morbidly Obese"))))
+
+(define (arithmetic-prog a_0 difference iNdex)
+  (if (equal? 0 iNdex)
+      '()
+      (cons a_0 (arithmetic-prog(+ a_0 difference) difference (- iNdex 1)))))
+
+(define (geometric-prog a_0 ratio iNdex)
+  ("Your definition goes here"))
+
